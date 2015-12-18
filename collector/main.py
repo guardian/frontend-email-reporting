c2a2078a0bad89d62232d16e98417c7763e671f9
Francis Carr <francis.carr@guardian.co.uk>
2015-12-18 15:09:35 +0000

    Add main.py which was missing from git

---
+++ collector/main.py
@@ -0,0 +1,44 @@
+import query
+import dynamodb
+
+import logging
+logger = logging.getLogger()
+
+
+def runCron(event, context):
+    """
+    :param event: Scheduled event in lambda
+    :param context: Context object from lambda
+    :return: None
+    """
+    sends = list(query.getLastNDaySends(2))
+    logger.info('Received event: {0}'.format(event))
+    for send in sends:
+        logger.info('Putting SEND to dynamo: {0}'.format(send))
+        dynamoResponse = dynamodb.putToSendTableWithNowDate(send)
+        logger.info('Response from DynamoDB: {0}'.format(dynamoResponse))
+
+
+def backfillDatabase():
+    """
+    Backfills the dynamoDB with dates
+    :return: None
+    """
+    sends = list(query.getLastNDaySends(2))
+    print 'Received {0} sends'.format(len(sends))
+    for send in sends:
+        print 'Putting SEND to dynamo: {0}'.format(send)
+        dynamoResponse = dynamodb.putToSendTableWithDate(send, send.sentDate)
+        print 'Response from DynamoDB: {0}'.format(dynamoResponse)
+
+
+def testCredentials(event, context):
+    sends = list(query.getLastNDaySends(2))
+    logger.info("Got {0} results".format(len(sends)))
+    print "Got {0} results".format(len(sends))
+    for send in sends:
+        print str(send)
+        logger.info(str(send))
+
+if __name__ == '__main__':
+    testCredentials(None, None)
